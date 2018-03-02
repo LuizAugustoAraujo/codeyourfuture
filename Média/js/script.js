104270d17d7c19ba291d7f2a8cdca6751aa4d7ae
Luiz Augusto <luizaraujo1112@gmail.com>
2018-03-01 22:36:16 -0300

    novaversao

--- Média/js/script.js
+++ Média/js/script.js
@@ -1,3 +1,23 @@
 /* Insira seu código em javascript abaixo */
 
 console.log('Olá Mastertech!');
+
+function search() {
+        let pesquisa = $('#buscar').val();
+        let filter = pesquisa.toUpperCase();
+        let p = $('.tituloPergunta');
+        let q = $('.quadro');
+        let a;
+
+        for (i = 0; i < p.length; i++) {
+            a = p[i];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                p[i].style.display = "";
+                q[i].style.display = "";
+
+            } else {
+                p[i].style.display = "none";
+                q[i].style.display = "none";
+            }
+        }
+           }
